41ed0ca23632183930c945d755bc989eb2ebab7d
Kaiser-Yang <624626089@qq.com>
2024-07-22 16:03:13 +0800

    Update initialization from the website of spring

---
+++ src/main/java/edu/cmipt/gcs/GcsApplication.java
@@ -0,0 +1,13 @@
+package edu.cmipt.gcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcsApplication.class, args);
+    }
+
+}
