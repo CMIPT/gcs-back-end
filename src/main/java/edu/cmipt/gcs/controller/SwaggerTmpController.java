32dc995fcf29d836af5006407fef3815a086a2db
Kaiser-Yang <624626089@qq.com>
2024-07-30 11:25:28 +0800

    Add spring-doc for restful api

---
+++ src/main/java/edu/cmipt/gcs/controller/SwaggerTmpController.java
@@ -0,0 +1,16 @@
+package edu.cmipt.gcs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SwaggerTmpController {
+
+    @GetMapping("/users/{id}")
+    public void getUser(@PathVariable Long id) {
+        // implementation
+    }
+}
