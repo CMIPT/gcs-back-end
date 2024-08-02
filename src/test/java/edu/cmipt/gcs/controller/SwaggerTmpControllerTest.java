b0d7e0db8988b974b6e553ef076bfb7fdba61f4d
Kaiser-Yang <Kaiser-Yang@users.noreply.github.com>
2024-08-02 08:51:26 +0000

    Apply Google Java Style Format

--- src/test/java/edu/cmipt/gcs/controller/SwaggerTmpControllerTest.java
+++ src/test/java/edu/cmipt/gcs/controller/SwaggerTmpControllerTest.java
@@ -1,21 +1,21 @@
 package edu.cmipt.gcs.controller;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SwaggerTmpControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+    @Autowired private MockMvc mvc;
 
     @Test
     public void testGetUser() throws Exception {
