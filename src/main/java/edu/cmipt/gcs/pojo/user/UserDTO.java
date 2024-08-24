1c003c4814da4846244df03acb6645f8cd3af599
Kaiser-Yang <Kaiser-Yang@users.noreply.github.com>
2024-08-24 03:19:44 +0000

    Apply Google Java Style Format

--- src/main/java/edu/cmipt/gcs/pojo/user/UserDTO.java
+++ src/main/java/edu/cmipt/gcs/pojo/user/UserDTO.java
@@ -17,15 +17,34 @@ import jakarta.validation.constraints.Size;
  */
 @Schema(description = "User Data Transfer Object")
 public record UserDTO(
-        @Schema(description = "Username", requiredMode = Schema.RequiredMode.REQUIRED, example = "admin") @Size(groups = {
-                UpdateGroup.class,
-                CreateGroup.class }, min = ConstantProperty.MIN_USERNAME_LENGTH, max = ConstantProperty.MAX_USERNAME_LENGTH, message = "{UserDTO.username.Size}") String username,
-
-        @Schema(description = "Email", requiredMode = Schema.RequiredMode.REQUIRED, example = "admin@cmipt.edu") @Email(groups = {
-                UpdateGroup.class, CreateGroup.class }, message = "{UserDTO.email.Email}") @NotBlank(groups = {
-                        UpdateGroup.class, CreateGroup.class }, message = "{UserDTO.email.NotBlank}") String email,
-
-        @Schema(description = "User Password (Unencrypted)", requiredMode = Schema.RequiredMode.REQUIRED, example = "123456") @Size(groups = {
-                UpdateGroup.class,
-                CreateGroup.class }, min = ConstantProperty.MIN_PASSWORD_LENGTH, max = ConstantProperty.MAX_PASSWORD_LENGTH, message = "{UserDTO.userPassword.Size}") String userPassword){
-}
+        @Schema(
+                        description = "Username",
+                        requiredMode = Schema.RequiredMode.REQUIRED,
+                        example = "admin")
+                @Size(
+                        groups = {UpdateGroup.class, CreateGroup.class},
+                        min = ConstantProperty.MIN_USERNAME_LENGTH,
+                        max = ConstantProperty.MAX_USERNAME_LENGTH,
+                        message = "{UserDTO.username.Size}")
+                String username,
+        @Schema(
+                        description = "Email",
+                        requiredMode = Schema.RequiredMode.REQUIRED,
+                        example = "admin@cmipt.edu")
+                @Email(
+                        groups = {UpdateGroup.class, CreateGroup.class},
+                        message = "{UserDTO.email.Email}")
+                @NotBlank(
+                        groups = {UpdateGroup.class, CreateGroup.class},
+                        message = "{UserDTO.email.NotBlank}")
+                String email,
+        @Schema(
+                        description = "User Password (Unencrypted)",
+                        requiredMode = Schema.RequiredMode.REQUIRED,
+                        example = "123456")
+                @Size(
+                        groups = {UpdateGroup.class, CreateGroup.class},
+                        min = ConstantProperty.MIN_PASSWORD_LENGTH,
+                        max = ConstantProperty.MAX_PASSWORD_LENGTH,
+                        message = "{UserDTO.userPassword.Size}")
+                String userPassword) {}
