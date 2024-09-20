2cecb31d9ab5c540f3946c772649ab294d01596f
Quentin9922 <quentincheung261@gmail.com>
2024-09-20 21:25:06 +0800

    Modify the tables and add fields
    In this submission, the original redundant table `t_user_repository`
    was deleted, and the current table `t_user_star_repository` was added
    to reflect the collection relationship.
    
    And now two fields are added to the repository table, `https_url` for the
    link under the https protocol and `ssh_url` for the link under the ssh
    protocol.
    
    See #46 and #48.

--- database/table/t_repository.sql
+++ database/table/t_repository.sql
@@ -7,6 +7,8 @@ CREATE TABLE public.t_repository (
   star integer DEFAULT 0 NOT NULL,
   fork integer DEFAULT 0 NOT NULL,
   watcher integer DEFAULT 0 NOT NULL,
+  https_url character varying(1024) NOT NULL,
+  ssh_url character varying(1024) NOT NULL,
   gmt_created timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
   gmt_updated timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
   gmt_deleted timestamp without time zone
@@ -19,6 +21,11 @@ COMMENT ON COLUMN public.t_repository.repository_name IS 'Name of the repository
 COMMENT ON COLUMN public.t_repository.repository_description IS 'Description of the repository.';
 COMMENT ON COLUMN public.t_repository.is_private IS 'Indicates if the repository is private.';
 COMMENT ON COLUMN public.t_repository.user_id IS 'ID of the user who owns the repository.';
+COMMENT ON COLUMN public.t_repository.star IS 'Number of stars the repository has received.';
+COMMENT ON COLUMN public.t_repository.fork IS 'Number of times the repository has been forked.';
+COMMENT ON COLUMN public.t_repository.watcher IS 'Number of users watching the repository.';
+COMMENT ON COLUMN public.t_repository.https_url IS 'Repository link under HTTPS protocol.';
+COMMENT ON COLUMN public.t_repository.ssh_url IS 'Repository link under SSH protocol.';
 COMMENT ON COLUMN public.t_repository.gmt_created IS 'Timestamp when the repository was created.';
 COMMENT ON COLUMN public.t_repository.gmt_updated IS 'Timestamp when the repository was last updated.';
 COMMENT ON COLUMN public.t_repository.gmt_deleted IS 'Timestamp when the repository was deleted.
