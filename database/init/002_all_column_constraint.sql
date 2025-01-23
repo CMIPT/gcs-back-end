ac8a921e5c114b4ea59f4369956b8d0b79ba21f9
Kaiser-Yang <624626089@qq.com>
2025-01-23 21:31:54 +0800

    Add unique constraints to some columns
    This will fix #90 .
    
    We don't delete the pre-checks because we want return a good
    feedback to the user. Keeping the pre-checks will help us to
    return a good feedback in most situations.

--- database/init/002_all_column_constraint.sql
+++ database/init/002_all_column_constraint.sql
@@ -1,19 +1,37 @@
 -- The constarint of the primary key and unique key is added to the table.
 ALTER TABLE ONLY public.t_repository
 ADD CONSTRAINT pk_repository PRIMARY KEY (id);
+ALTER TABLE ONLY public.t_repository
+ADD CONSTRAINT unique_t_repository_name_user_id
+UNIQUE (repository_name, user_id, gmt_deleted);
 
 -- The constraint of t_user is added to the table.
 ALTER TABLE ONLY public.t_user
 ADD CONSTRAINT pk_user_table PRIMARY KEY (id);
+ALTER TABLE ONLY public.t_user
+ADD CONSTRAINT unique_t_user_username
+UNIQUE (username, gmt_deleted);
+ALTER TABLE ONLY public.t_user
+ADD CONSTRAINT unique_t_user_email
+UNIQUE (email, gmt_deleted);
 
 -- The constraint of t_user_star_repository is added to the table.
 ALTER TABLE ONLY public.t_user_star_repository
 ADD CONSTRAINT pk_user_star_repository PRIMARY KEY (id);
+ALTER TABLE ONLY public.t_user_star_repository
+ADD CONSTRAINT unique_t_user_star_repository_user_id_repository_id
+UNIQUE (user_id, repository_id, gmt_deleted);
 
--- The constraint of the primary key and unique key is added to the table.
+-- The constraint of t_ssh_key is added to the table.
 ALTER TABLE ONLY public.t_ssh_key
 ADD CONSTRAINT pk_ssh_key PRIMARY KEY (id);
+ALTER TABLE ONLY public.t_ssh_key
+ADD CONSTRAINT unique_t_ssh_key_user_id_name_key
+UNIQUE (user_id, name, public_key, gmt_deleted);
 
 -- The constraint of t_user_collaborate_repository is added to the table.
 ALTER TABLE ONLY public.t_user_collaborate_repository
 ADD CONSTRAINT pk_user_collaborate_repository PRIMARY KEY (id);
+ALTER TABLE ONLY public.t_user_collaborate_repository
+ADD CONSTRAINT t_user_collaborate_repository_collaborator_id_repository_id
+UNIQUE (collaborator_id, repository_id, gmt_deleted);
