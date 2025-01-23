2a2b5406e4c7dfa3a68366448650c621810813bc
Kaiser-Yang <58209855+Kaiser-Yang@users.noreply.github.com>
2025-01-23 08:00:30 +0000

    Apply Google Java Style Format

--- src/main/java/edu/cmipt/gcs/config/RedisConfig.java
+++ src/main/java/edu/cmipt/gcs/config/RedisConfig.java
@@ -14,6 +14,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
     @Value("${spring.redis.host}")
     private String redisHost;
+
     @Value("${spring.redis.port}")
     private Integer redisPort;
 
