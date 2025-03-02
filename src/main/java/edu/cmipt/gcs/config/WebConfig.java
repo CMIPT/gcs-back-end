d3ad54f121cd81b7ebe5a262d238721d0d11ff3c
Kaiser-Yang <624626089@qq.com>
2025-03-02 14:38:29 +0800

    Use nginx to finixh https reverse proxy

--- src/main/java/edu/cmipt/gcs/config/WebConfig.java
+++ src/main/java/edu/cmipt/gcs/config/WebConfig.java
@@ -1,16 +1,13 @@
 package edu.cmipt.gcs.config;
 
-import edu.cmipt.gcs.constant.ApiPathConstant;
 import edu.cmipt.gcs.constant.ApplicationConstant;
 import edu.cmipt.gcs.constant.HeaderParameter;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -19,9 +16,6 @@ import java.util.Arrays;
 
 @Configuration
 public class WebConfig {
-    @Value("${front-end.url}")
-    private String frontEndUrl;
-
     @Bean
     @Profile(ApplicationConstant.DEV_PROFILE)
     FilterRegistrationBean<CorsFilter> corsFilterDev() {
@@ -32,26 +26,6 @@ public class WebConfig {
         addExposedHeader(config);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean<CorsFilter> bean =
-                new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(Ordered.LOWEST_PRECEDENCE);
-        return bean;
-    }
-
-    @Bean
-    @Profile({ApplicationConstant.PROD_PROFILE, ApplicationConstant.TEST_PROFILE})
-    FilterRegistrationBean<CorsFilter> corsFilterNonDev() {
-        CorsConfiguration config = new CorsConfiguration();
-        if (frontEndUrl != null && frontEndUrl.length() > 0) {
-            config.addAllowedOrigin(frontEndUrl);
-            config.addAllowedMethod(HttpMethod.GET);
-            config.addAllowedMethod(HttpMethod.POST);
-            config.addAllowedMethod(HttpMethod.DELETE);
-            config.addAllowedHeader("*");
-            addExposedHeader(config);
-        }
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration(ApiPathConstant.ALL_API_PREFIX + "/**", config);
         FilterRegistrationBean<CorsFilter> bean =
                 new FilterRegistrationBean<>(new CorsFilter(source));
         bean.setOrder(Ordered.LOWEST_PRECEDENCE);
