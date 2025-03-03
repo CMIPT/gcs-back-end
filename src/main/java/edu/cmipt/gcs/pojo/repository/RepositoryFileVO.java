40674b640cb4aa0ceaf022b9a2372b99390fbc0a
Kaiser-Yang <624626089@qq.com>
2025-03-03 13:12:38 +0800

    Fix documentation

--- src/main/java/edu/cmipt/gcs/pojo/repository/RepositoryFileVO.java
+++ src/main/java/edu/cmipt/gcs/pojo/repository/RepositoryFileVO.java
@@ -3,7 +3,7 @@ package edu.cmipt.gcs.pojo.repository;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record RepositoryFileVO(
-        @Schema(description = "File or Directory Path, relative to the root") String path,
+        @Schema(description = "File or Directory Path, relative to the request path") String path,
         @Schema(description = "Is file or directory") Boolean isDirectory
 ) {
 }
