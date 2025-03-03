924b7a77462f47ea175c98ef694345b86ec387a8
Kaiser-Yang <624626089@qq.com>
2025-03-03 14:02:31 +0800

    Update path --> name of RepositoryFileVO

--- src/main/java/edu/cmipt/gcs/pojo/repository/RepositoryFileVO.java
+++ src/main/java/edu/cmipt/gcs/pojo/repository/RepositoryFileVO.java
@@ -3,7 +3,7 @@ package edu.cmipt.gcs.pojo.repository;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record RepositoryFileVO(
-        @Schema(description = "File or Directory Path, relative to the request path") String path,
+        @Schema(description = "File or Directory name") String name,
         @Schema(description = "Is file or directory") Boolean isDirectory
 ) {
 }
