a9dd2bcf5c650cc83c740d4e55023b1e955b6e82
Kaiser-Yang <624626089@qq.com>
2025-06-03 23:29:32 +0800

    test(repository): test for new API

--- src/test/java/edu/cmipt/gcs/constant/TestConstant.java
+++ src/test/java/edu/cmipt/gcs/constant/TestConstant.java
@@ -18,6 +18,7 @@ public class TestConstant {
   public static String OTHER_REFRESH_TOKEN;
   public static String REPOSITORY_ID;
   public static String REPOSITORY_NAME;
+  public static String REPOSITORY_LATEST_COMMIT_HASH;
   public static Integer REPOSITORY_SIZE = 10;
   public static String OTHER_REPOSITORY_ID;
   public static String OTHER_PRIVATE_REPOSITORY_ID;
